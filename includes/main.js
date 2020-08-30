cade5c222ccc81457c55b1b33a5d2e1079fd0d70
Kelvin Morrison <kelvinninja1@gmail.com>
2020-08-30 17:58:24 +0000

    Added: Dialing and default modal view template

---
+++ includes/main.js
@@ -0,0 +1,36 @@
+var count = 0;
+var digits = '';
+
+$(".digit").on('click', function() {
+  var num = ($(this).clone().children().remove().end().text());
+  if (count < 11) {
+    $("#output").append('<span>' + num.trim() + '</span>');
+    digits += num.trim();
+    count++
+  }
+});
+
+$('.fa-long-arrow-left').on('click', function() {
+  $('#output span:last-child').remove();
+  count--;
+})
+
+function dial(target){
+  if (target == '*558#') {
+    $("#mainUSSDModal").modal("show");
+  } else {
+    $("#noServiceModal").modal("show");
+  }
+
+}
+
+function dialInput(){
+  // digits = $("#output").val();
+  if (digits != '') {
+    alert("Digits are "+ digits);
+    dial(digits);
+  } else {
+    alert("There is nothing");
+  }
+
+}
