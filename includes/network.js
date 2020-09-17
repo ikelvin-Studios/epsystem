ce6ab541c46b3170a420bd32e2de4ea96b6e8566
Kelvin Morrison <kelvinninja1@gmail.com>
2020-09-17 11:27:35 +0000

    Added: Network Endpoint Simulation

--- includes/network.js
+++ includes/network.js
@@ -3,19 +3,23 @@ var currentConnection = '';
 var sampleMSISDN = '0549500940';
 
 function networkAccess(request){
+
   if (currentConnection == '') {
-    if (request == '*961#') {
-      request = '*961#';
-      currentConnection = '*961#';
-      endpointAccess(currentConnection, sampleMSISDN, request, 1);
+    if (checkEndpointAccess(request)) {
+      request = request;
+      currentConnection = request;
       $("#mainUSSDModal").modal("show");
+      endpointAccess(currentConnection, sampleMSISDN, request, 1);
+
     } else {
       $("#noServiceModal").modal("show");
     }
   } else {
+    // $("#mainUSSDModal").modal("hide");
+    // $("#mainUSSDModal").modal("show");
     endpointAccess(currentConnection, sampleMSISDN, request, 0);
-  }
 
+  }
 
   return true;
 }
